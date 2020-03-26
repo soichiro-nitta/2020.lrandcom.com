5d3cbd6d0f28d7b3a31924abfaefdac51e5fa0dc
soichiro-nitta <soic1226@gmail.com>
2020-03-26 15:47:09 +0900

    feat: 主要取引先のセクションを追加

--- src/components/index/OurClient.tsx
+++ src/components/index/OurClient.tsx
@@ -2,24 +2,67 @@ import React from 'react'
 import styled from 'styled-components'
 import styles from '~/utils/styles'
 import Title from '~/components/index/title'
+import config from '~/utils/config'
 
 type ContainerProps = {
   className: string
 }
-type ComponentProps = {} & ContainerProps
+type ComponentProps = {
+  client: string[]
+  last: number
+} & ContainerProps
 
 const Component: React.FC<ComponentProps> = props => (
   <div className={props.className}>
-    <Title className="title">主要取引先</Title>
+    <div className="head">
+      <Title className="title">{config.index.ourClient.title}</Title>
+      <div className="notes">{config.index.ourClient.notes}</div>
+    </div>
+    <div className="body">
+      {props.client.map((companyName, index) => (
+        <React.Fragment key={index}>
+          <span className="companyName">{companyName}</span>
+          {index !== props.last && <span className="slash"> / </span>}
+        </React.Fragment>
+      ))}
+    </div>
   </div>
 )
 
 const StyledComponent = styled(Component)`
   ${styles.mixins.flexCenter};
+  > * > .notes {
+    margin-top: 4.5rem;
+    opacity: 0.65;
+  }
+  > .body {
+    ${styles.mixins.lhCrop(3)};
+    margin-left: 7.5vw;
+    padding: 5rem;
+    width: 37.5vw;
+    height: 18.75;
+    background: #1f1f1f;
+    border: 1px solid #171717;
+  }
+  > .body > span {
+    font-size: 1.4rem;
+    letter-spacing: 0.1rem;
+  }
+  > .body > .companyName {
+    letter-spacing: 0.1rem;
+    font-weight: bold;
+    opacity: 0.65;
+  }
+  > .body > .slash {
+    letter-spacing: 0.5rem;
+    opacity: 0.3;
+  }
 `
 
 const Container: React.FC<ContainerProps> = props => {
-  return <StyledComponent {...props} />
+  const client = config.index.ourClient.client
+  const last = client.length - 1
+  return <StyledComponent client={client} last={last} {...props} />
 }
 
 export default Container
