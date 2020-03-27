fe4e3cf120eb5f2f1ea7e8b0f7485cf72c880afc
soichiro-nitta <soic1226@gmail.com>
2020-03-27 14:12:09 +0900

    feat: カードコンポーネントを作成

---
+++ src/components/index/card.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import styles from '~/utils/styles'
+
+type ContainerProps = {
+  className: string
+}
+type ComponentProps = {} & ContainerProps
+
+const Component: React.FC<ComponentProps> = props => (
+  <div className={props.className}>{props.children}</div>
+)
+
+const StyledComponent = styled(Component)`
+  padding: 5rem;
+  font-size: 1.4rem;
+  font-weight: bold;
+  letter-spacing: 0.1rem;
+  background: #1f1f1f;
+  border: 1px solid #181818;
+`
+
+const Container: React.FC<ContainerProps> = props => {
+  return <StyledComponent {...props} />
+}
+
+export default Container
