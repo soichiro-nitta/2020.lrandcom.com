54692acf24632c3919549e3f552f43e0a6594310
soichiro-nitta <soic1226@gmail.com>
2020-04-27 13:28:00 +0900

    feat: heading1のline-height

--- src/components/base/Heading1.tsx
+++ src/components/base/Heading1.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styled from 'styled-components'
-import { styles } from '~/utils/styles'
 
 type ContainerProps = {
   className: string
@@ -12,9 +11,9 @@ const Component: React.FC<ComponentProps> = props => (
 )
 
 const StyledComponent = styled(Component)`
-  ${styles.mixins.lhCrop(1.6)};
   font-size: 4.3rem;
   font-weight: bold;
+  line-height: 1;
   letter-spacing: 0.4rem;
   transform: skew(-5deg);
   text-decoration: underline;
