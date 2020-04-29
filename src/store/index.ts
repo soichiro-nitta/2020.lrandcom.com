e76c2af4fa257f432f78f56719db306a13df8877
soichiro-nitta <soic1226@gmail.com>
2020-04-29 15:02:50 +0900

    feat: 不要なストア除去

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,19 +1,15 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit'
 
 import header from './header'
-import article from './article'
 import navigation from './navigation'
 import media from './media'
 import window from './window'
-import articles from './articles'
 
 const reducer = combineReducers({
-  article,
   header,
   navigation,
   media,
-  window,
-  articles
+  window
 })
 
 export type StateTypes = ReturnType<typeof reducer>
