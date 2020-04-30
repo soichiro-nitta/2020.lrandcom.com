3eb42e9830d3796d3abc758571a4c1b0f31b1202
soichiro-nitta <soic1226@gmail.com>
2020-05-01 00:18:14 +0900

    feat: update about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -23,44 +23,40 @@ type ComponentProps = {
 const Component: React.FC<ComponentProps> = props => (
   <div className={props.className}>
     <Logo className="logo" />
-    {props.sp && <div className="margin line" />}
+    <div className="margin line" />
     <Lead className="wrapper">
       リーディング＆カンパニーは東京を拠点とするマーケティング会社です。マーケティング記事の作成を中心にサイト制作、動画制作などを大企業から中小企業の法人様にご提供しております。
     </Lead>
-    {props.sp && <div className="margin line" />}
+    <div className="margin line" />
     <Thumbnail className="margin thumbnail" />
-    {props.sp && <div className="margin line" />}
+    <div className="margin line" />
     <Outline className="wrapper" />
     <div className="margin line" />
-    {props.sp && <div className="margin line" />}
+    <div className="margin line" />
     <OurClient className="wrapper" />
-    {props.sp && <div className="margin line" />}
+    <div className="margin line" />
     <Email className="margin thumbnail" />
-    {props.sp && <div className="margin line" />}
+    <div className="margin line" />
     <Lead className="wrapper">
       多極的な視点で物事を考えられるライターを常に募集しています。キャリアや経験はハシゴではなくジャングルジム。常識とは異なった視点で、ライティングを通じ、未来の解像度を上げていって下さい。読書好き、旅行好き、運動好きであればパーフェクト!
     </Lead>
     <div className="wrapper">
       <Button className="button">詳しくはこちらから</Button>
     </div>
-    {props.sp && <div className="margin line" />}
-    {props.sp && <div className="margin line" />}
+    <div className="margin line" />
+    <div className="margin line" />
   </div>
 )
 
 const StyledComponent = styled(Component)`
-  padding: 15.5rem 0;
-  ${styles.media.sp} {
-    padding: 0;
-  }
   > .margin {
-    margin-top: 7rem;
+    margin-top: 6rem;
     ${styles.media.sp} {
       margin-top: 3rem;
     }
   }
   > .wrapper {
-    margin: 7rem auto 0;
+    margin: 6rem auto 0;
     width: 70rem;
     ${styles.media.sp} {
       margin: 3rem auto 0;
@@ -68,6 +64,7 @@ const StyledComponent = styled(Component)`
     }
   }
   > .logo {
+    margin-top: 15.5rem;
     width: 100%;
     height: 47.5rem;
     ${styles.media.sp} {
