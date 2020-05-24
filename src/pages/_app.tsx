ccde3761743945525d1bfc1884a62cb23831d69f
soichiro-nitta <soic1226@gmail.com>
2020-05-24 14:18:11 +0900

    feat: Google Analyticsを使用

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,17 @@
-import * as React from 'react'
+import React from 'react'
 import { Provider } from 'react-redux'
 import store from '~/store'
 import { AppProps } from 'next/app'
 import Layout from '~/layouts/default'
+import Router from 'next/router'
+import { gtag } from '~/lib/gtag'
 
 const _App: React.FC<AppProps> = props => (
   <Provider store={store}>
     <Layout {...props} />
   </Provider>
 )
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url))
 
 export default _App
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '~/lib/gtag'
 
 class MyDocument extends Document {
   render(): React.ReactElement {
@@ -11,6 +12,22 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Noto+Sans+JP&display=swap"
           />
           <link rel="stylesheet" href="https://use.typekit.net/ubu5lwf.css" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
         </Head>
         <body>
           <Main />
